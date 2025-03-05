f6c978524b80a98fa98e18a5fc0e7e915651a8af
pbrose82 <34488965+pbrose82@users.noreply.github.com>
2025-03-05 16:22:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ HTML_TEMPLATE = '''
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>COA OCR Extractor</title>
+    <title>Alchemy COA Intelligence</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body { 
